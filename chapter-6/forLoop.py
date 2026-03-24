58f274487b630cdc01748c372fe3c8304de6d3d5
rozer <shubhamsinha07084@gmail.com>
2026-03-24 19:14:39 +0530

    loops

---
+++ chapter-6/forLoop.py
@@ -0,0 +1,14 @@
+foodlist = ["cake", "mango", "pizza"]
+
+for item in foodlist:
+    print(item)
+    
+collegeList = ("NIT DELHI", "IIT-DELHI")
+for eachItem in collegeList:
+    print("College visited is", eachItem)
+    
+print("\n----------------\n")
+
+# for loop with range function
+for i in range (1, 10, 2): # 1, 3, 5, 7, 9
+    print(i)
---
+++ chapter-6/whileLoops.py
@@ -0,0 +1,20 @@
+# while and for loop
+
+num = 1
+while num <=10: # as long as condition keep running
+    print("shubham")
+    num = num +1
+    
+print("\nNow we are out of loop\n")
+
+# q1> print 1 to 10 using while loop
+i = 0
+while (i<10):
+    print(i)
+    i+=1
+    
+# q2>  print 10 to 1 using while loop   
+j = 10
+while (j>0):
+    print(j)
+    j-=1
