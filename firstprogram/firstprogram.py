07c2d9d30d7d6c0f3787ae1389e579aa70862422
rozer <shubhamsinha07084@gmail.com>
2026-03-24 10:26:08 +0530

    firstprogram

---
+++ firstprogram/firstprogram.py
@@ -0,0 +1,8 @@
+print("my name is shubham sinha")
+
+# assignment - python program to print name 10 times
+# each time in a new line
+
+# use backslash n to print in new line - \n
+
+print(("my name is shubham sinha" + "\n") * 10)
